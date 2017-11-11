e66cca4e25fc27af2344f8f7fd4b0b50a1b90baa
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2017-11-11 12:19:27 +0100

    Extract calculations to methods by non statically way

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,11 +2,20 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
         double fahrenheit, reamur;
         for (int celsius = 0; celsius <= 100; celsius += 10) {
-            fahrenheit = 9.0/5.0 * celsius + 32.0;
-            reamur = 4.0/5.0 * celsius;
+            fahrenheit = main.celsiusToFahrenheit(celsius);
+            reamur = main.celsiusToReamur(celsius);
             System.out.println(celsius + ", " + fahrenheit + ", " + reamur);
         }
+    }
+
+    private double celsiusToReamur(int celsius) {
+        return 4.0/5.0 * celsius;
+    }
+
+    private double celsiusToFahrenheit(int celsius) {
+        return 9.0/5.0 * celsius + 32.0;
     }
 }
